dc51b215e6f9a9b0922a7c019c7a0379fefbae10
William <wiliamwijaya1985@gmail.com>
2021-01-09 16:18:55 +0700

    Change base shape and improve centerPadding algorithm.

--- Java Swing/src/com/application/Swing/PieceBox.java
+++ Java Swing/src/com/application/Swing/PieceBox.java
@@ -76,48 +76,23 @@ public class PieceBox {
 		int temp[] = new int[2];
 		if (this.piece.getShape() == Tetrominoes.NoShape) return temp;
 		
-		int pMin[] = {this.piece.getX(0), this.piece.getY(0)};
-		int pMax[] = {this.piece.getX(0), this.piece.getY(0)};
-		if (this.piece.getShape() != Tetrominoes.NoShape) {
-			for (int i = 1; i < 4; ++i) {
-				pMin[0] = Math.min(this.piece.getX(i), pMin[0]);
-				pMin[1] = Math.min(this.piece.getY(i), pMin[1]);
-				pMax[0] = Math.max(this.piece.getX(i), pMax[0]);
-				pMax[1] = Math.max(this.piece.getY(i), pMax[1]);
-			}
-		}
-		int pWidth = squareWidth * (pMax[0] - pMin[0] + 1);
-		int pHeight = squareWidth * (pMax[1] - pMin[1] + 1);
-		
 		temp[0] = x;
 		temp[1] = y + titleBarHeight;
 		if (this.piece.getShape() == Tetrominoes.LineShape) {
-			// center == middle-middle
+			// center == middle
 			temp[0] += this.width/4;
-			temp[1] += (this.height - pHeight)/2;
-		} 
-		else if (this.piece.getShape() == Tetrominoes.TShape) {
-			// center == middle-left
-			temp[0] += (this.width - pWidth)/2 + squareHeight;
-			temp[1] += this.height/4;
-		} 
-		else if (this.piece.getShape() == Tetrominoes.LShape) {
-			// center == middle-right
-			temp[0] += this.width/2;
-			temp[1] += (this.height - pHeight)/2 + squareHeight;
+			temp[1] += (this.height - squareHeight)/2;
 		} 
 		else if (this.piece.getShape() == Tetrominoes.SquareShape) {
 			// center == upper-left
-			temp[0] += (this.width - pWidth)/2;
-			temp[1] += (this.height - pHeight)/2;
+			temp[0] += (this.width - 2 * squareWidth)/2;
+			temp[1] += (this.height - 2 * squareHeight)/2;
 		}
 		else {
-			// center == middle-left
-			// shape == S, mirroredL, OR Z
-			temp[0] += (this.width - pWidth)/2;
-			temp[1] += (this.height - pHeight)/2 + squareHeight;
-		} 
-		
+			// center == lower-middle
+			temp[0] += (this.width - squareWidth)/2;
+			temp[1] += this.height/2;
+		}
 		return temp;
 	}
 
